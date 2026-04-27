e64808bd252bdcb162c10792a8b689ab57d8653c
Shasan Dhakal <63613238+shasan11@users.noreply.github.com>
2026-04-27 14:28:34 +0545

    Connect accounting frontend CRUD pages to Laravel APIs

--- app/Http/Controllers/AccountingController.php
+++ app/Http/Controllers/AccountingController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
+use App\Models\BankAccount;
+use App\Models\Branch;
+use App\Models\ChartOfAccount;
+use App\Models\Currency;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -9,22 +14,38 @@ class AccountingController extends Controller
 {
     public function chartOfAccounts(): Response
     {
-        return Inertia::render('App/Accounting/ChartOfAccounts/Index');
+        return Inertia::render('App/Accounting/ChartOfAccounts/Index', [
+            'branches' => Branch::query()->orderBy('name')->get(['id', 'name']),
+            'accounts' => Account::query()->orderBy('name')->get(['id', 'name', 'code']),
+            'currencies' => Currency::query()->orderBy('code')->get(['id', 'name', 'code']),
+        ]);
     }
 
     public function bankAccounts(): Response
     {
-        return Inertia::render('App/Accounting/BankAccounts/Index');
+        return Inertia::render('App/Accounting/BankAccounts/Index', [
+            'branches' => Branch::query()->orderBy('name')->get(['id', 'name']),
+            'currencies' => Currency::query()->orderBy('code')->get(['id', 'name', 'code']),
+            'accounts' => Account::query()->orderBy('name')->get(['id', 'name', 'code']),
+        ]);
     }
 
     public function cashTransfers(): Response
     {
-        return Inertia::render('App/Accounting/CashTransfers/Index');
+        return Inertia::render('App/Accounting/CashTransfers/Index', [
+            'branches' => Branch::query()->orderBy('name')->get(['id', 'name']),
+            'currencies' => Currency::query()->orderBy('code')->get(['id', 'name', 'code']),
+            'bankAccounts' => BankAccount::query()->orderBy('display_name')->get(['id', 'display_name', 'code']),
+        ]);
     }
 
     public function journalVouchers(): Response
     {
-        return Inertia::render('App/Accounting/JournalVouchers/Index');
+        return Inertia::render('App/Accounting/JournalVouchers/Index', [
+            'branches' => Branch::query()->orderBy('name')->get(['id', 'name']),
+            'currencies' => Currency::query()->orderBy('code')->get(['id', 'name', 'code']),
+            'accounts' => ChartOfAccount::query()->orderBy('name')->get(['id', 'name', 'code']),
+        ]);
     }
 
     public function quickBills(): Response
