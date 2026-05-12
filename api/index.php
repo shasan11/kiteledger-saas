73549bd1dcd81ed4ec075f73e125075035949af4
Shasan Dhakal <shasandkl4@gmail.com>
2026-05-12 22:09:16 +0545

    Vercel thing

--- api/index.php
+++ api/index.php
@@ -1,3 +1,14 @@
 <?php
 
+$source = __DIR__ . '/../database/database.sqlite';
+$target = '/tmp/database.sqlite';
+
+if (! file_exists($target)) {
+    if (file_exists($source)) {
+        copy($source, $target);
+    } else {
+        touch($target);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
